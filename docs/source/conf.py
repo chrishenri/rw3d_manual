4342a8e5f8daabb43529151e12005ec1cbb03e86
chrishenri <christopher.v.henri@gmail.com>
2024-06-07 10:46:04 +0200

    change latex formating

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,13 +48,17 @@ epub_show_urls = 'footnote'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    'papersize': 'letterpaper',
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    'pointsize': '11pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    'preamble': r'''
+        \usepackage{charter}
+        \usepackage[defaultsans]{lato}
+        \usepackage{inconsolata}
+    ''',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
--- docs/source/processes.rst
+++ docs/source/processes.rst
@@ -6,8 +6,7 @@ Simulated processes
 Transport
 ----------------
 
-RW3D solve the typical transport processes that are: advection, dispersion, and diffusion. This is done by solving the RWPT equation defined in the chapter :ref:`randomwalk`. 
-
+RW3D solve the typical transport processes that are: advection, dispersion, and diffusion. This is done by solving the RWPT scheme defined in the chapter :ref:`randomwalk`. 
 
 Scheme of interpolation of velocities. 
 
